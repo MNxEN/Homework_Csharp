7a76171f6e0d41e27c91c65ed6f4710af8a28d50
Mikhail Chekushkin <rbmihey@mail.ru>
2023-06-12 20:17:59 +0500

    Add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число: ");
+string strVal = Console.ReadLine();
+int number = Convert.ToInt32(strVal);
+int count = 1;
+if (number < count)
+{
+    Console.WriteLine($"Число должно быть больше {count}!");
+}
+else
+{
+    while (count <= number)
+    {
+        if (count % 2 == 0)
+        {
+            Console.Write($"{count} ");
+        }
+        count++;
+    }
+}
